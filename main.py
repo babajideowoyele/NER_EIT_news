d3ea0b5d08839d8ee38dd61fa996aee4a8943c40
Bhuvanesh Verma <workoholic24x7@gmail.com>
2023-08-01 14:44:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# streamlit_app.py
+import streamlit as st
+import pandas as pd
+import spacy_streamlit
+
+models = ["en_core_web_sm", "en_core_web_trf", "en_core_web_lg"]
+
+st.title("EIT News")
+df = pd.read_csv('data/eit_news - EIT news.csv')
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(df)
+visualizers = ["ner", "tokens"]
+curr_doc = st.slider('Select any instance', 0,len(df), 0)
+
+spacy_streamlit.visualize(models, df.loc[curr_doc, 'body'], visualizers=visualizers)
